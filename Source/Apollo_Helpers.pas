004b745580d07b685b52475ae26de2da9b200278
alezzle <niekras@tut.by>
2021-09-09 16:51:49 +0300

    dev

--- Source/Apollo_Helpers.pas
+++ Source/Apollo_Helpers.pas
@@ -22,12 +22,51 @@ type
     function Count: Integer;
   end;
 
+  TIntArrHelper = record helper for TArray<Integer>
+    function CommaText: string;
+    function Count: Integer;
+    procedure SplitCommaText(const aText: string);
+  end;
+
 implementation
 
 uses
   System.Hash,
   System.IOUtils,
   System.SysUtils;
+
+{TIntArrHelper}
+
+function TIntArrHelper.Count: Integer;
+begin
+  Result := Length(Self);
+end;
+
+procedure TIntArrHelper.SplitCommaText(const aText: string);
+var
+  Value: string;
+  Values: TArray<string>;
+begin
+  Self := [];
+  Values := aText.Split([',']);
+
+  for Value in Values do
+    Self := Self + [Value.ToInteger];
+end;
+
+function TIntArrHelper.CommaText: string;
+var
+  Value: Integer;
+begin
+  Result := '';
+
+  for Value in Self do
+  begin
+    if not Result.IsEmpty then
+      Result := Result + ',';
+    Result := Result + Value.ToString;
+  end;
+end;
 
 {TStrArrHelper}
 
